5f7e673a519ab4e71bacda5c2379c8fc53d7b4c1
savam <savam@nordeus.eu>
2014-03-08 17:42:17 +0100

    Added visual indicator that match has changed on live table

--- public/js/matches.js
+++ public/js/matches.js
@@ -5,7 +5,10 @@ var LiveMatches = React.createClass({displayName: 'LiveMatches',
 			url: 'api/live', dataType: 'json',
 			success: function(data) {
 				if (data.matches) {
-					this.setState({rows: data.matches});
+					this.setState({
+						rows: data.matches,
+						oldRows: this.state.rows
+					});
 				} else {
 					console.log(data.error);
 				}
@@ -14,15 +17,25 @@ var LiveMatches = React.createClass({displayName: 'LiveMatches',
 		});
 	},
 	getInitialState: function() {
-		return {rows: []};
+		return {rows: [], oldRows: []};
 	},
 	componentWillMount: function() {
 		this.loadMatches();
 	},
+	getMatchInOldRows: function(id) {
+		var rows = this.state.oldRows;
+		if (rows.length > 0) {
+			for(var i=0, len=rows.length; i<len; i++) {
+				if(rows[i].id == id) return rows[i];
+			}
+		}
+		return null;
+	},
 	render: function() {
 		var matcheRows = this.state.rows.map(function (matchRow, index) {
-			return MatchRow( {key:matchRow.id, index:index, data:matchRow} );
-		});
+			var oldRow = this.getMatchInOldRows(matchRow.id);
+			return MatchRow( {key:matchRow.id, index:index, data:matchRow, oldData:oldRow} );
+		}.bind(this));
 		return (
 			React.DOM.table( {id:"matches"}, 
 			React.DOM.tr(null, 
@@ -39,8 +52,16 @@ var LiveMatches = React.createClass({displayName: 'LiveMatches',
 });
 var MatchRow = React.createClass({displayName: 'MatchRow',
 	render: function() {
+		var rowId = 'match' + this.props.data.id;
+		var classes = React.addons.classSet({
+			'matchRow': true,
+			'updatedRow': this.props.oldData && (
+					this.props.oldData.home_score != this.props.data.home_score
+				||	this.props.oldData.away_score != this.props.data.away_score
+			)
+		});
 		return (
-			React.DOM.tr( {className:"matchRow", title:"Click to view match information"}, 
+			React.DOM.tr( {className:classes, id:rowId, title:"Click to view match information"}, 
 				React.DOM.td(null, this.props.index+1,"."),
 				React.DOM.td(null, this.props.data.home_name),
 				React.DOM.td( {className:"goalCell"}, this.props.data.home_score,":",this.props.data.away_score),
